2304ac112b23934936bd9132879b37124aa5f837
Raziloo <waissraz27@gmail.com>
2023-04-04 12:27:07 +0300

    Added database

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+require('dotenv').config();
 
 const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [];
-
 
 const app = express();
 
@@ -16,14 +17,51 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB Atlas successfully!");
+}).catch(err => console.error(err));
 
-app.get("/", function(req, res){
+const postSchema = {
+  title: String,
+  content: String
+};
 
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts
-  });
-  
+const Post = mongoose.model("Post", postSchema);
+
+// app.get("/", function(req, res){
+
+//   Post.find({})
+//   .then(posts => {
+
+//     res.render("home", {
+//       startingContent: homeStartingContent,
+//       posts: posts
+//       });
+
+//   });
+//   });
+
+app.get("/", async function(req, res){
+
+  try {
+    const posts = await Post.find({});
+    
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: []
+    });
+  }
+
 });
 
 app.get("/about", function(req, res){
@@ -41,38 +79,34 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  
+  post.save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.error(err);
+    });  
 });
 
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = req.params.postName.replace("-", " ").toLowerCase();
+app.get("/posts/:postId", function(req, res){
 
-  posts.forEach(function(post){
-    const storedTitle = post.title.toLowerCase();
-    if (storedTitle === requestedTitle) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId})
+    .then(post => {
       res.render("post", {
         title: post.title,
         content: post.content
-        
       });
-    }
-    else {
-      console.log("No match found!");
-    }
-  });
+    })
+    .catch(err => console.error(err));
+
 });
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
